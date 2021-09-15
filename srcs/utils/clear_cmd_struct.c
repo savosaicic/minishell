9680bc973f9d71c666066641704180ffeb3a3b7a
sasaicic <sasaicic@student.42.fr>
2021-09-15 14:11:04 +0200

    add comment

--- srcs/utils/clear_cmd_struct.c
+++ srcs/utils/clear_cmd_struct.c
@@ -1,9 +1,16 @@
 
 #include "minishell.h"
 
+/*
+**	this function must me call by ft_lstclear()
+**	void *cmd_struct NEED to point on a t_cmd structure
+*/
 void	clear_cmd_struct(void *cmd_struct)
 {
 	int	i;
+
+	if (!cmd_struct)
+		return ;
 
 	i = 0;
 	while (((t_cmd *)cmd_struct)->args[i] != NULL)
