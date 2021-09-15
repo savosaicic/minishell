0896698402c617dec097804b2d83f369d9b3b874
Jessica BOISSERAND <jboisser@e2r5p17.clusters.42paris.fr>
2021-09-15 13:27:17 +0200

    improve: test.sh

--- test.sh
+++ test.sh
@@ -10,114 +10,119 @@ BLUE="\033[34m"
 MAGENTA="\033[35m"
 
 clear
-printf "${YELLOW} ============== UNIT TESTS MINISHELL ==============\n${NC}"
+count_tests=0
+count_valid_test=0
+printf "${YELLOW}=========== INTEGRATION TESTS MINISHELL ===========\n\n${NC}"
 
-printf "${ULINE}Testing for basic COMMANDS\n${NC}"
 
-printf "○ echo /bin/ls ................................... "
-string=$(echo "/bin/ls" | ./minishell)
-# echo "/bin/ls" | ./minishell
-string_expected=$(/bin/ls)
-printf "\n${string}\n"
-echo "-------------------"
-printf "${string_expected}\n"
-# printf "\n${string_expected\n}"
-if [ *"$string"* == "$string_expected" ]
+
+printf "${ULINE}${BOLD}Testing for basic COMMANDS\n${NC}" ################################
+
+printf "○ ls ........................................... "
+let "count_tests=count_tests+1"
+string=$(echo "ls" | ./minishell)
+string_expected=$(ls)
+if [[ "$string" == *"$string_expected"* ]]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
 
-printf "○ echo -n hello ................................ "
-string=$(echo "echo -n hello" | ./minishell)
-string_expected=$(echo -n hello)
-if [ "$string" == "$string_expected" ]
+printf "○ ls -l ........................................ "
+let "count_tests=count_tests+1"
+string=$(echo "ls -l" | ./minishell)
+string_expected=$(ls -l)
+if [[ "$string" == *"$string_expected"* ]]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
-
 printf "\n"
 
-
-printf "${ULINE}Testing for basic BUILTINS\n${NC}"
+printf "${ULINE}${BOLD}Testing for basic BUILTINS\n${NC}" ################################
 
 printf "○ echo hello ................................... "
+let "count_tests=count_tests+1"
 string=$(echo "echo hello" | ./minishell)
 string_expected=$(echo hello)
 if [ "$string" == "$string_expected" ]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
 
 printf "○ echo -n hello ................................ "
+let "count_tests=count_tests+1"
 string=$(echo "echo -n hello" | ./minishell)
 string_expected=$(echo -n hello)
 if [ "$string" == "$string_expected" ]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
-
-printf "\n"
 
 # printf "○ cd ../ ................................ "
 # printf "○ cd /Users ................................ "
 
 printf "○ pwd .......................................... "
+let "count_tests=count_tests+1"
 string=$(echo pwd | ./minishell)
 string_expected=$(pwd)
-if [ $string == $string_expected ]
+if [ "$string" == "$string_expected" ]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
 
-printf "\n"
-
-
 printf "○ export ....................................... "
+let "count_tests=count_tests+1"
 unset VAR
 echo export VAR=variable | ./minishell > /dev/null 2>&1
 string=$VAR
 if [ "$string" == "variable" ]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 	export_is_valid=1
 else
 	printf "${RED}KO\n${NC}"
 	export_is_valid=0
 fi
 
-printf "\n"
-
 printf "○ unset ........................................ "
+let "count_tests=count_tests+1"
 echo unset VAR | ./minishell > /dev/null 2>&1
 string=$VAR
 if [[ -z "$string" && $export_is_valid -eq 1 ]]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
 
-printf "\n"
-
 printf "○ env .......................................... "
+let "count_tests=count_tests+1"
 string=$(echo "echo env" | ./minishell)
 string_expected=$(env)
 if [ "$string" == "$string_expected" ]
 then
 	printf "${GREEN}OK\n${NC}"
+	let "count_valid_test=count_valid_test+1"
 else
 	printf "${RED}KO\n${NC}"
 fi
 
 # printf "○ exit ....................... \n"
 
+printf "\n\n${BOLD}${ULINE}${RED}TOTAL: ${count_valid_test}/${count_tests}\n${NC}"
