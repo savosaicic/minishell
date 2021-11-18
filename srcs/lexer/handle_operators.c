34bdbcab85818654dbcd73e6913827e454531186
sasaicic <sasaicic@student.42.fr>
2021-11-18 15:16:40 +0100

    save

--- srcs/lexer/handle_operators.c
+++ srcs/lexer/handle_operators.c
@@ -14,8 +14,23 @@ int	handle_quote(char **cmd_buffer, char *str, t_list **token_lst)
 	quote = **cmd_buffer;
 	(*cmd_buffer)++;
 	i = 0;
+
+//	while (**cmd_buffer != quote && **cmd_buffer)
+//		buffer[i++] = *(*cmd_buffer)++;
+
+	ft_bzero(buffer, 4096);
 	while (**cmd_buffer != quote && **cmd_buffer)
-		buffer[i++] = *(*cmd_buffer)++;
+	{
+		if (**cmd_buffer == '$' && *(*cmd_buffer + 1) && *(*cmd_buffer + 1) != '$' && *(*cmd_buffer + 1) != ' ')
+		{
+
+		}
+
+		buffer[i] = **cmd_buffer;
+		(*cmd_buffer)++;
+		i++;
+	}
+
 	if (!**cmd_buffer)
 		return (1);
 
@@ -24,6 +39,9 @@ int	handle_quote(char **cmd_buffer, char *str, t_list **token_lst)
 		buffer[i++] = *(*cmd_buffer)++;
 	buffer[i] = '\0';
 
+	printf("\n>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n");
+	printf("\t\"%s\"\n", buffer);
+	printf("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\n");
 	if (str)
 		ft_lstadd_back(token_lst, ft_lstnew(write_token(ft_strjoin(str, buffer))));
 	else
