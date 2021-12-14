83610152bab6b5253010b620be025ca4a3ee8f1e
sasaicic <sasaicic@student.42.fr>
2021-12-14 09:12:11 +0100

    clean useless functions

--- srcs/builtins/colons.c
+++ srcs/builtins/colons.c
@@ -6,24 +6,11 @@
 /*   By: sasaicic <sasaicic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 09:08:19 by sasaicic          #+#    #+#             */
-/*   Updated: 2021/12/14 09:08:30 by sasaicic         ###   ########.fr       */
+/*   Updated: 2021/12/14 09:11:37 by sasaicic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-void	print_cmd_lst(t_list *cmd_lst)
-{
-	while (cmd_lst)
-	{
-		printf("[");
-		for (int i=0; ((t_cmd *)cmd_lst->content)->args[i]; i++)
-			printf("%s; ", ((t_cmd *)cmd_lst->content)->args[i]);
-		printf("]\n");
-		printf("#############################\n");
-		cmd_lst = cmd_lst->next;
-	}
-}
 
 int	do_nothing(void)
 {
--- srcs/utils/debug.c
+++ srcs/utils/debug.c
@@ -6,7 +6,7 @@
 /*   By: sasaicic <sasaicic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 09:08:19 by sasaicic          #+#    #+#             */
-/*   Updated: 2021/12/14 09:08:30 by sasaicic         ###   ########.fr       */
+/*   Updated: 2021/12/14 09:11:32 by sasaicic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,19 @@ void	print_token(t_list *list)
 	printf("\n");
 	printf("__________________________\n\n");
 	free_tab(types);
+}
+
+void	print_cmd_lst(t_list *cmd_lst)
+{
+	while (cmd_lst)
+	{
+		printf("[");
+		for (int i=0; ((t_cmd *)cmd_lst->content)->args[i]; i++)
+			printf("%s; ", ((t_cmd *)cmd_lst->content)->args[i]);
+		printf("]\n");
+		printf("#############################\n");
+		cmd_lst = cmd_lst->next;
+	}
 }
 
 void	print_tab(char **tab)
