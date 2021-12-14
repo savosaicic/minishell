ca752075e5a466b04d65177387fd1eb752142710
sasaicic <sasaicic@student.42.fr>
2021-12-14 10:44:46 +0100

    refacto: clear.c

--- srcs/utils/clear.c
+++ srcs/utils/clear.c
@@ -6,7 +6,7 @@
 /*   By: sasaicic <sasaicic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 09:08:19 by sasaicic          #+#    #+#             */
-/*   Updated: 2021/12/14 10:12:45 by sasaicic         ###   ########.fr       */
+/*   Updated: 2021/12/14 10:24:06 by sasaicic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,8 +59,8 @@ void	clear_prg_struct(void)
 {
 	if (!g_prg)
 		return ;
-	free(rl_line_buffer);
-	rl_line_buffer = NULL;
+	free(g_prg->cmd_buffer);
+	g_prg->cmd_buffer = NULL;
 	ft_lstclear(&g_prg->env_lst, clear_var_struct);
 	ft_lstclear(&g_prg->cmd_lst, clear_cmd_struct);
 	free(g_prg->env_lst);
