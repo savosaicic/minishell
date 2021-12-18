a7e52ec979b04edab6a1c6c9d35a907e372f3c90
jessicaBoisserand <boisserand.jessica@orange.fr>
2021-12-18 11:50:33 +0100

    merge: refactoring_jess on master

--- srcs/builtins/exit_shell.c
+++ srcs/builtins/exit_shell.c
@@ -12,21 +12,31 @@
 
 #include "minishell.h"
 
-int	exit_shell(t_cmd *cmd)
+int	exit_shell(t_cmd *cmd, char *exit_str)
 {
-	if (ft_charlen(cmd->args) > 2)
+	pid_t	pid;
+
+	pid = 0;
+	if (!exit_str)
+		pid = fork();
+	if (!pid)
 	{
-		ft_putstr_fd("exit\n", 2);
-		return (puterror("exit", "too many arguments", 1));
+		if (ft_charlen(cmd->args) > 2)
+		{
+			ft_putstr_fd(exit_str, 2);
+			return (puterror("exit", "too many arguments", 1));
+		}
+		else if (cmd->args[1] && !ft_str_isdigit(cmd->args[1]))
+		{
+			ft_putstr_fd(exit_str, 2);
+			exit_failure("exit", "numeric argument required", 2);
+		}
+		else if (cmd->args[1])
+			exit_success(ft_atoi(cmd->args[1]), exit_str != NULL);
+		else
+			exit_success(0, exit_str != NULL);
 	}
-	else if (cmd->args[1] && !ft_str_isdigit(cmd->args[1]))
-	{
-		ft_putstr_fd("exit\n", 2);
-		exit_failure("exit", "numeric argument required", 2);
-	}
-	else if (cmd->args[1])
-		exit_success(ft_atoi(cmd->args[1]), TRUE);
-	else
-		exit_success(0, TRUE);
-	return (1);
+	if (!exit_str)
+		return (wait_pid(pid));
+	return (0);
 }
--- srcs/utils/signal.c
+++ srcs/utils/signal.c
@@ -12,37 +12,37 @@
 
 #include "minishell.h"
 
-void	sig_int(int sig __attribute__((unused)))
+void    sig_int(int sig __attribute__((unused)))
 {
-	if (!g_prg->child)
-	{
-		ft_putstr_fd("$> ", 2);
-		ft_putstr_fd(rl_line_buffer, 2);
-		ft_putstr_fd("  \b\b\n", 2);
-		rl_replace_line("", 0);
-		rl_on_new_line();
-		rl_redisplay();
-	}
-	else if (g_prg->pid)
-		ft_putstr_fd("\n", 2);
+    if (!g_prg->child)
+    {
+        ft_putstr_fd("$> ", 2);
+        ft_putstr_fd(rl_line_buffer, 2);
+        ft_putstr_fd("  \b\b\n", 2);
+        rl_replace_line("", 0);
+        rl_on_new_line();
+        rl_redisplay();
+    }
+    else if (g_prg->pid)
+        ft_putstr_fd("\n", 2);
 }
 
-void	sig_quit(int sig __attribute__((unused)))
+void    sig_quit(int sig __attribute__((unused)))
 {
-	if (!g_prg->child)
-	{
-		ft_putstr_fd("$> ", 2);
-		ft_putstr_fd(rl_line_buffer, 2);
-		ft_putstr_fd("  \b\b", 2);
-	}
-	else if (g_prg->pid)
-		ft_putstr_fd("\n", 2);
+    if (!g_prg->child)
+    {
+        ft_putstr_fd("$> ", 2);
+        ft_putstr_fd(rl_line_buffer, 2);
+        ft_putstr_fd("  \b\b", 2);
+    }
+    else if (g_prg->pid)
+        ft_putstr_fd("\n", 2);
 }
 
-void	watch_signals(void)
+void    watch_signals(void)
 {
-	if (signal(SIGINT, &sig_int) == SIG_ERR)
+    if (signal(SIGINT, &sig_int) == SIG_ERR)
 		exit_failure(NULL, strerror(errno), 1);
-	if (signal(SIGQUIT, &sig_quit) == SIG_ERR)
+    if (signal(SIGQUIT, &sig_quit) == SIG_ERR)
 		exit_failure(NULL, strerror(errno), 1);
 }
